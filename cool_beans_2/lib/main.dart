cbbc3eed36a7380c0a61adeca187518fd9fc2cad
Paxtonian <146684739+Paxtonian@users.noreply.github.com>
2023-10-09 02:10:16 +0000

    october 8th work

--- cool_beans_2/lib/main.dart
+++ cool_beans_2/lib/main.dart
@@ -38,21 +38,51 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),  // ← Example change.
-          Text(appState.current.asLowerCase),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,  // ← Add this.
+          children: [
+            Text('A random AWESOME idea:'),
+            BigCard(pair: pair),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+      
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                    ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
+class BigCard extends StatelessWidget {
+  final WordPair pair;
 
-        ],
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+    color: theme.colorScheme.onPrimary,);
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        )
       ),
     );
   }
